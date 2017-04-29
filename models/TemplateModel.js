b3c246cda87a6ec05f3f13640c7c257be73fc9ec
Siddhartha <sidart101@gmail.com>
2017-04-29 23:56:27 +0530

    changed Template model for new functionalites

--- models/TemplateModel.js
+++ models/TemplateModel.js
@@ -1,10 +1,29 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var sectionSchema = new Schema({
+	viewMode: String,
+	backgroundColor: String,
+	formElements: []
+});
+
 var templateSchema = new Schema({ 
-							"title": String ,
-							"viewMode": String,
-							"backgroundColor": String,
+							"title": {
+								"text": String,
+								"align": String,
+								"size": { "type": String, "default": "1" }
+							} ,
+							"sections": [{
+								"heading": {
+									"text": String,
+									"align": String,
+									"size": { "type": String, "default": "2" }
+								},
+								"name": String,
+								"viewMode": String,
+								"backgroundColor": String,
+								"formElements": { "type" : Array , "default" : [] }
+							}],
 							"createdBy": Schema.ObjectId,
 							"createdAt": Date,
 						    "updatedAt": { type: Date, default: Date.now },
